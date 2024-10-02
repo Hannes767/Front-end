7780310fe363742cf556bb87116280417158a252
Hannes767 <65671608+Hannes767@users.noreply.github.com>
2024-10-02 12:44:06 +0300

    tagasiside meetodid 02.10.24

--- tagasiside/src/pages/Kasutajad.js
+++ tagasiside/src/pages/Kasutajad.js
@@ -27,6 +27,44 @@ function Kasutajad() {
     setLucioIndex(index); // Uuenda Lucio indeksi state'i
   }
 
+  const findFirstCLetter = () => {
+    const kasutaja = kasutajad.find(user => user.name.substring(0,1) === "C");
+    console.log(kasutaja);
+  }
+
+  const sortUserLat = () => {
+    kasutajad.sort((a,b) => a.address.geo.lat.localeCompare(b.address.geo.lat));
+    console.log(kasutajad);
+    uuendaKasutajad(kasutajad.slice());
+  }
+
+  const filterUserLangPos = () => {
+    const result = kasutajad.filter(kasutaja => kasutaja.address.geo.lng > 0);
+    uuendaKasutajad(result);
+  }
+
+  const sumUserId = () => {
+    let sum = 0;
+    kasutajad.forEach(kasutaja => {sum += kasutaja.id}); // sama kui sum = sum + kasutaja.id
+    console.log(sum);
+  }
+
+  const addToPhoneNumber = () => {
+    const result = kasutajad.map(kasutaja => ({...kasutaja, phone: "000 " + kasutaja.phone}))
+    uuendaKasutajad(result);
+  }
+
+  const replaceAllEmails = () => {
+    const result = kasutajad.map(user => user.email);
+    console.log(result);
+  }
+
+  const replaceCatchPhraseLetters = () => {
+    const result = kasutajad.map(user => ({...user, company:{...user.company, catchPhrase: user.company.catchPhrase.replaceAll("a", "e")}}))
+      uuendaKasutajad(result);
+      console.log(result);
+  }
+
   return (
     <div>
       <Link to="/">
@@ -38,6 +76,13 @@ function Kasutajad() {
       <button onClick={() => filterName()}>Kasutaja username pikkus on suurem-võrdne 10</button>
       <button onClick={() => findIndexLucio()}>Lucio index on: {lucioIndex}</button>
       {lucioIndex !== null && <div>Lucio indeks: {lucioIndex}</div>} {/* Kuvab Lucio indeksi, kui see on leitud */}
+      <button onClick={() => findFirstCLetter()}>Kasutaja nimi algab C tähega</button>
+      <button onClick={() => sortUserLat()}>Kasutajate sortimine lat järgi</button>
+      <button onClick={() => filterUserLangPos()}>Kasutajate sortimine pos lng järgi</button>
+      <button onClick={() => sumUserId()}>Kasutajate id-de liitmine</button>
+      <button onClick={() => addToPhoneNumber()}>Kasutajate telefoninumbritele 000 liitmine</button>
+      <button onClick={() => replaceAllEmails()}>Kasutajate emailide eraldamine uude massiivi</button>
+      <button onClick={() => replaceCatchPhraseLetters()}>Kasutajate catchphrasede tähtede asendamine</button>
 
 
       <br /><br />
