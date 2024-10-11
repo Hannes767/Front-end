c3f87ee2d7b4b85654e295d380e65e9a139b8912
Hannes <65671608+Hannes767@users.noreply.github.com>
2024-10-11 16:27:53 +0300

    bakery-shop 11.10.24

--- bakery-shop/src/pages/Employees.jsx
+++ bakery-shop/src/pages/Employees.jsx
@@ -1,15 +1,42 @@
+import React, { useEffect, useRef, useState } from 'react'
 import { Button, Table } from "react-bootstrap";
 
 function Employees() {
+  const [employees, setEmployees] = useState([]);
+  const idRef = useRef();
+  const firstNameRef = useRef();
+  const lastNameRef = useRef();
+  const emailRef = useRef();
+  const avatarRef = useRef()
 
   // TODO: Load data from backend service
 
+  useEffect(() => {
+    fetch("https://reqres.in/api/users")
+      .then(res => res.json())
+      .then(body => setEmployees(body.data))
+  }, []);
+
   const addEmployee = () => {
+    employees.push(
+      {
+        "id": idRef.current.value,
+        "email": emailRef.current.value,
+        "first_name": firstNameRef.current.value,
+        "last_name" : lastNameRef.current.value,        
+        "avatar": avatarRef.current.value,
+      }
+    );
+    localStorage.setItem("employee", JSON.stringify(employees));
+    setEmployees(employees.slice());
     // TODO: Add validations
     // TODO: Add an employee to the table
   }
 
   const deleteEmployee = (index) => {
+    employees.splice(index, 1);
+    localStorage.setItem("employee", JSON.stringify(employees));
+    setEmployees(employees.slice());
     // TODO: Delete an employee from the table
   }
 
@@ -23,28 +50,30 @@ function Employees() {
           <th scope="col">Name</th>
           <th scope="col">Email</th>
           {/* <!-- TODO: Add a column for an avatar --> */}
+          <th scope="col">Avatar</th>
           <th scope="col">Actions</th>
         </tr>
         </thead>
         <tbody>
-        <tr>
-          <td>123</td>
-          <td>Added name 1</td>
-          <td>email@email.com</td>
-          <td><Button type="button" variant="danger">Delete</Button></td>
-        </tr>
-        <tr>
-          <td>124</td>
-          <td>Added name 2</td>
-          <td>email2@email.com</td>
-          <td><Button type="button" variant="danger">Delete</Button></td>
-        </tr>
+          {employees.map((employee, index) =>
+        <tr key={index}>
+          <td>{employee.id}</td>
+          <td>{employee.first_name} {employee.last_name}</td>
+          <td>{employee.email}</td>
+          <td><img style={{width: "100px"}} src={employee.avatar} alt="" /></td>
+          <td onClick={() => deleteEmployee(index)}><Button type="button" variant="danger">Delete</Button></td>
+        </tr>)}
+        
 
         <tr className="input-row">
-          <td><input type="text" placeholder="ID" className="form-control"/></td>
-          <td><input type="text" placeholder="Name" className="form-control"/></td>
-          <td><input type="text" placeholder="Email" className="form-control"/></td>
-          <td><Button type="submit" variant="success">Add</Button></td>
+          <td><input ref={idRef} type="text" placeholder="ID" className="form-control"  /></td>
+          <td>
+            <input ref={firstNameRef} type="text" placeholder="First Name" className="form-control"/>
+            <input ref={lastNameRef} type="text" placeholder="Last Name" className="form-control"/>
+          </td>
+          <td><input ref={emailRef} type="text" placeholder="Email" className="form-control"/></td>
+          <td><input ref={avatarRef} type="text"  placeholder="Avatar" className="form-control"/></td>
+          <td onClick={addEmployee}><Button type="submit" variant="success">Add</Button></td>
         </tr>
         </tbody>
       </Table>
