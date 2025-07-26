7c0456ddcd55f323b4043a01ad26bebc9a98d6f5
Hannes <65671608+Hannes767@users.noreply.github.com>
2025-07-26 18:15:50 +0300

    Update local.js

--- kutsekoolid-server/local.js
+++ kutsekoolid-server/local.js
@@ -1,6 +1,6 @@
 // local.js
 import app from "./index.js"; // või "./server.js"
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
   console.log(`Lokaalne test töötab: http://localhost:${port}`);
